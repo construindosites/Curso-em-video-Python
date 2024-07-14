cbc6293747785b720301c7f16e42d64cec1ed1ff
Douglas Gaspar Alves <artedga@gmail.com>
2024-07-14 11:49:39 -0300

    extras

---
+++ ex/ex015.py
@@ -0,0 +1,5 @@
+dias = int(input('Quantos dias foram alugados? '))
+km = int(input('Quantos kilometros vc andou? '))
+diaria = dias * 60
+valorkm = km * 0.15
+print('Valor a pagar', diaria + valorkm)
