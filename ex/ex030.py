80c13ce717c646906a86a606783fbe78e7630114
Douglas Gaspar Alves <artedga@gmail.com>
2024-07-27 06:50:40 -0300

    up

--- ex/ex030.py
+++ ex/ex030.py
@@ -3,5 +3,5 @@ result = n % 2
 if result == 0:
     print('O número {} é PAR'.format(n))
 else:
-    print('O número {} é IMPAR'.format(n))
-    
+    print('O número {} é IMPAR'.format(n)) 
+        
